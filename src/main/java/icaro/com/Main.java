071052da35c0207980d113b0c33520a846d49504
anbel <anbelmaririvas@gmail.com>
2023-09-12 00:13:31 -0300

    Proyecto base

---
+++ src/main/java/icaro/com/Main.java
@@ -0,0 +1,17 @@
+package icaro.com;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!\n");
+
+        System.out.printf("amo a mi gordito");
+
+        System.out.println(12345);
+        System.out.println(123.15);
+        System.out.println('c');
+        System.out.println("verdadero");
+        System.out.println("false");
+}
+
+}
